afe4ebff9f3671f55981b5592deadca5f32a7bf2
allamurodxakimov <xakimov2806@gmail.com>
2023-09-27 21:21:02 -0700

    if2 3

--- if03.py
+++ if03.py
@@ -8,4 +8,16 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    if a>b and b>c:
+        return b
+    if b>a and a>c:
+        return a
+    if b>c and c>a:
+        return c
+    if a>c and c>b:
+        return c
+    if c>a and a>b:
+        return a
+    if c>b and b>a:
+        return b
+print (main(2,3,4))
