da3eb736a95f9439872433acaa49d4bca97bb333
allamurodxakimov <xakimov2806@gmail.com>
2023-09-27 21:22:01 -0700

    if2 5

--- if05.py
+++ if05.py
@@ -6,4 +6,19 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    a1 = n//10000
+    a2 = n%10000//1000
+    a3 = n%1000//100
+    a4 = n%100//10
+    a5 = n%10
+    if a1>a2 and a1>a3 and a1>a4 and a1>a5:
+        return a1
+    if a2>a1 and a2>a3 and a2>a4 and a2>a5:
+        return a2
+    if a3>a2 and a3>a1 and a3>a4 and a3>a5:
+        return a3
+    if a4>a2 and a4>a3 and a4>a1 and a4>a5:
+        return a4
+    if a5>a2 and a5>a3 and a5>a4 and a5>a1:
+        return a5
+print(main(12345))
