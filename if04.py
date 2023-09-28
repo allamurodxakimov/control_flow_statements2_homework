0e22a90907b747f624cb7c0df55453c0e99da013
allamurodxakimov <xakimov2806@gmail.com>
2023-09-27 21:21:33 -0700

    if2 4

--- if04.py
+++ if04.py
@@ -7,4 +7,12 @@ def main(a,b):
     Returns:
         int: return answer.
     """
-    return
+    if a==b:
+        return 0
+    if a!=b:
+        if a>b:
+            return a
+        if b>a :
+            return b
+
+print(main(456,543))
